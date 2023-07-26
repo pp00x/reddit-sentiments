6c00ef098661df294d72d158adf160ce4f1a43e1
pp00x <officialprashantpatil@outlook.com>
2023-07-26 21:27:42 +0530

    Update router to accomodate response schemas

--- server/src/routes/sentiment/router.py
+++ server/src/routes/sentiment/router.py
@@ -6,12 +6,13 @@ from datetime import datetime
 from src.database import Session
 from src.service.sentiment_analyzer import analyze_and_store
 from src.cache import cache_analysis, get_cached_analysis
+from src.routes.sentiment.schemas import AnalysisResponse
 
 
 sentiment_router: APIRouter = APIRouter()
 
 
-@sentiment_router.get("/sentiment/r/{subreddit}", dependencies=[Depends(RateLimiter(times=6, seconds=60))])
+@sentiment_router.get("/sentiment/r/{subreddit}", response_model=AnalysisResponse, dependencies=[Depends(RateLimiter(times=6, seconds=60))])
 async def get_sentiment(subreddit: str, background_tasks: BackgroundTasks):
     cached_sentiment_analysis = await get_cached_analysis(f"{subreddit}{datetime.utcnow().date()}")
     if cached_sentiment_analysis:
